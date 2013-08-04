133f0022c30e71f42dddab134e293510afedb9f9
Matej Kramny <matejkramny@gmail.com>
2013-08-04 16:41:41 +0100

    Add (forgotten) geolocation, redundant Attendee model (now in Event)

--- models/Geolocation.js
+++ models/Geolocation.js
@@ -4,18 +4,18 @@ var schema = mongoose.Schema;
 var ObjectId = schema.ObjectId;
 
 var scheme = schema({
+	geo: {
+		lat: { type: Number },
+		lng: { type: Number }
+	},
 	event: {
 		type: ObjectId,
 		ref: 'Event'
-	},
-	user: {
-		type: ObjectId,
-		ref: 'User'
-	},
-	message: {
-		type: ObjectId,
-		ref: 'Message'
 	}
 });
 
-exports.Attendee = mongoose.model("Attendee", scheme);
+scheme.index({
+	geo: '2d'
+});
+
+exports.Geolocation = mongoose.model("Geolocation", scheme);
