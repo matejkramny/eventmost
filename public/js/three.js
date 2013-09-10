1d87823a0f523b12178da70f829bd327dd1bcdab
Patryk Brzeziński <info@studiograficzne24.pl>
2013-09-10 20:05:57 +0200

    no message

---
+++ public/js/three.js
@@ -0,0 +1,21 @@
+// DOM Ready
+$(function() {
+
+            $(".dropdown dt a").click(function() {
+                $(".dropdown dd ul").toggle();
+            });
+                        
+            $(".dropdown dd ul li a").click(function() {
+                var text = $(this).html();
+                $(".dropdown dt a ").html(text);
+                $(".dropdown dd ul").hide();
+            });
+                        
+        
+
+            $(document).bind('click', function(e) {
+                var $clicked = $(e.target);
+                if (! $clicked.parents().hasClass("dropdown"))
+                    $(".dropdown dd ul").hide();
+            });
+        });
