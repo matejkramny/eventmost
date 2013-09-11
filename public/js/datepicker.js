9fe5dcd23e5d74fcfaceb6d66e4eaf936a36fa35
Patryk Brzeziński <info@studiograficzne24.pl>
2013-09-11 10:36:34 +0200

    no message

---
+++ public/js/datepicker.js
@@ -0,0 +1,15 @@
+// DOM Ready
+$(function() {
+
+            $('#datepicker').datepicker({
+                inline: true,
+                //nextText: '&rarr;',
+                //prevText: '&larr;',
+                showOtherMonths: true,
+                //dateFormat: 'dd MM yy',
+                dayNamesMin: ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'],
+                //showOn: "button",
+                //buttonImage: "img/calendar-blue.png",
+                //buttonImageOnly: true,
+            });
+        });
