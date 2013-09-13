a4302d89d388d39a87ec410ea092db9bf5425d91
Patryk Brzeziński <info@studiograficzne24.pl>
2013-09-13 23:48:51 +0200

    Layout small fixing,

---
+++ public/js/four.js
@@ -0,0 +1,6 @@
+jQuery(document).ready(function() {
+    jQuery('#mycarousel').jcarousel({
+        vertical: true,
+        scroll: 2
+    });
+});
--- public/js/three.js
+++ public/js/three.js
@@ -6,14 +6,56 @@ $(document).ready(function() {
 	
 	$(".dropdown dd ul li a").click(function() {
 		var text = $(this).html();
-		$(this).parentsUntil('.dropdown').parent().find("dt a").html(text);
-		$(this).parentsUntil('ul').parent().hide();
+		$(this).parent().parent().parent().find("dt a").html(text);
+		$(this).parent().parent().hide();
 	});
 	
 	$(document).bind('click', function(e) {
 		var $clicked = $(e.target);
-		if (! $clicked.parent().hasClass("dropdown")) {
+		if (! $clicked.parents().hasClass("dropdown")) {
 			$(".dropdown dd ul").hide();
+		}
+	});
+});
+
+
+$(document).ready(function() {
+	// Dropdown toggles
+	$(".dropdown2 dt a").click(function() {
+		$(this).parent().parent().find("dd ul").toggle();
+	});
+	
+	$(".dropdown2 dd ul li a").click(function() {
+		var text = $(this).html();
+		$(this).parent().parent().parent().find("dt a").html(text);
+		$(this).parent().parent().hide();
+	});
+	
+	$(document).bind('click', function(e) {
+		var $clicked = $(e.target);
+		if (! $clicked.parents().hasClass("dropdown2")) {
+			$(".dropdown2 dd ul").hide();
+		}
+	});
+});
+
+
+$(document).ready(function() {
+	// Dropdown toggles
+	$(".dropdown3 dt a").click(function() {
+		$(this).parent().parent().find("dd ul").toggle();
+	});
+	
+	$(".dropdown3 dd ul li a").click(function() {
+		var text = $(this).html();
+		$(this).parent().parent().parent().find("dt a").html(text);
+		$(this).parent().parent().hide();
+	});
+	
+	$(document).bind('click', function(e) {
+		var $clicked = $(e.target);
+		if (! $clicked.parents().hasClass("dropdown3")) {
+			$(".dropdown3 dd ul").hide();
 		}
 	});
 });
