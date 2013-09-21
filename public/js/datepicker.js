9b7c3dd4d69874b2e6940b960c3726b108ea883a
Matej Kramny <matejkramny@gmail.com>
2013-09-21 17:32:52 +0100

    Datepicker automatic date update

--- public/js/datepicker.js
+++ public/js/datepicker.js
@@ -1,30 +1,27 @@
 // DOM Ready
-$(function() {
-
-            $('#datepicker').datepicker({
-                inline: true,
-                //nextText: '&rarr;',
-                //prevText: '&larr;',
-                showOtherMonths: true,
-                //dateFormat: 'dd MM yy',
-                dayNamesMin: ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'],
-                //showOn: "button",
-                //buttonImage: "img/calendar-blue.png",
-                //buttonImageOnly: true,
-            });
-        });
-
-$(function() {
-
-            $('#datepicker2').datepicker({
-                inline: true,
-                //nextText: '&rarr;',
-                //prevText: '&larr;',
-                showOtherMonths: true,
-                //dateFormat: 'dd MM yy',
-                dayNamesMin: ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'],
-                //showOn: "button",
-                //buttonImage: "img/calendar-blue.png",
-                //buttonImageOnly: true,
-            });
-        });
+$(document).ready(function() {
+	function onselect(date, $this) {
+		var dateString = date.getDate() + "/" + date.getMonth() + "/" + date.getFullYear();
+		$($this).parent().find(".datepickerResult").html(dateString);
+	}
+	
+	$('#datepicker, #datepicker2').each(function() {
+		$(this).datepicker({
+			inline: true,
+			//nextText: '&rarr;',
+			//prevText: '&larr;',
+			showOtherMonths: true,
+			//dateFormat: 'dd MM yy',
+			dayNamesMin: ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'],
+			//showOn: "button",
+			//buttonImage: "img/calendar-blue.png",
+			//buttonImageOnly: true,
+			onSelect: function() {
+				var date = $(this).datepicker('getDate');
+				onselect(date, $(this));
+			}
+		});
+		var date = $(this).datepicker('getDate');
+		onselect(date, $(this));
+	});
+});
