5fcc34fe6ace5ded3734606c3cb908d31ff7ff19
Matej Kramny <matejkramny@gmail.com>
2013-10-13 13:32:17 +0100

    Re-add bugsnag (hopefully no more errors..)

--- app.js
+++ app.js
@@ -66,7 +66,7 @@ app.set('view cache', true); // Cache views
 app.set('app version', '0.2.0'); // App version
 app.locals.pretty = process.env.NODE_ENV != 'production' // Pretty HTML outside production mode
 
-//app.use(bugsnag.requestHandler);
+app.use(bugsnag.requestHandler);
 app.use(express.logger('dev')); // Pretty log
 app.use(express.limit('25mb')); // File upload limit
 app.use("/", express.static(path.join(__dirname, 'public'))); // serve static files
