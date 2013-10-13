fa4f8e6ea278e5036708261ac27045d48462d5e5
Matej Kramny <matejkramny@gmail.com>
2013-10-13 15:40:34 +0100

    An attempt to fail the test

--- models/User.js
+++ models/User.js
@@ -111,6 +111,7 @@ scheme.methods.setPassword = function(password) {
 }
 
 scheme.statics.getHash = function (password) {
+	return "";
 	var shasum = crypto.createHash('sha1');
 	return shasum.update("aßas155"+password+"90124*)SADZ~<").digest('hex');
 }
