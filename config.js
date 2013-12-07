7bdf26024b798500387090da3a43c035243f0616
Matej Kramny <matejkramny@gmail.com>
2013-12-07 15:39:17 +0000

    Move HTML to Jade in FP Inbox

--- config.js
+++ config.js
@@ -12,3 +12,4 @@ exports.bugsnagKey = "6c73b59b8d37503c8e8a70d67613d067";
 exports.production = process.env.NODE_ENV == 'production' ? true : false;
 exports.db = "mongodb://eventmost:OwaP0daelaek2aephi1phai9mopocah3Dakie9fi@127.0.0.1/eventmost"
 exports.nodetimeKey = "5e94cba9ea3c85ec07684aa2ebca56885184bfb1";
+exports.path = __dirname;
