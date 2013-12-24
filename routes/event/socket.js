6caa47e09dd1c52940c848b07e3bc4d5d728ffa1
Matej Kramny <matejkramny@gmail.com>
2013-12-24 00:54:03 +0000

    Sorry, forgot one file

---
+++ routes/event/socket.js
@@ -0,0 +1,76 @@
+var mongoose = require('mongoose')
+	, messages = require('./messages')
+	, models = require('../../models')
+	, io = require('../../app').io // has to fetch it
+
+setTimeout(function() {
+	io = require('../../app').io;
+}, 500)
+
+exports.socket = function (sock) {
+	sock.on('register event.comments', registerEventComments)
+}
+
+function registerEventComments (data) {
+	var sock = this;
+	var user = sock.handshake.user;
+	var ev = data.event;
+	if (!ev) {
+		return;
+	}
+	
+	try {
+		ev = mongoose.Types.ObjectId(ev);
+	} catch (e) {
+		return;
+	}
+	
+	// check if event exists, and if the socket has access to it..
+	models.Event.getEvent(data.event, function(ev) {
+		if (!ev) { return; }
+		
+		var attending = false;
+		var isAdmin = false;
+		var theAttendee;
+		
+		for (var i = 0; i < ev.attendees.length; i++) {
+			var attendee = ev.attendees[i];
+			
+			if (typeof attendee.user === "object" && attendee.user._id.equals(user._id)) {
+				attending = true;
+				isAdmin = attendee.admin;
+				theAttendee = attendee;
+				break;
+			}
+		}
+		
+		if (!attending) {
+			return;
+		}
+		
+		sock.set('event', ev._id);
+		sock.set('eventattending', attending);
+		sock.set('eventadmin', isAdmin);
+		sock.set('attendee', theAttendee._id);
+	}, true)
+}
+
+exports.notifyComment = function (ev, msg, responseTo) {
+	var sockets = io.sockets.clients();
+	
+	console.log("Getting")
+	
+	for (var i = 0; i < sockets.length; i++) {
+		var s = sockets[i];
+		
+		s.get('event', function(err, value) {
+			console.log(err)
+			console.log(value);
+			if (err || !value) return;
+			
+			if (ev._id.equals(value)) {
+				s.emit('comment', msg)
+			}
+		})
+	}
+}
