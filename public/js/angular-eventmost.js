6e172e1fae775197663917e52f157e7db773ac0a
Matej Kramny <matejkramny@gmail.com>
2014-01-22 15:49:41 +0000

    Optimised JS

--- public/js/angular-eventmost.js
+++ public/js/angular-eventmost.js
@@ -1,1 +1,16 @@
 var eventMost = angular.module('eventMost', [])
+
+/* Font-Change */
+ $(document).ready(function () {
+     $(".font-change").each(function () {
+         var charLength = $(this).text().length;
+			
+         if (charLength >= 20) $(this).css("font-size", "12px");
+     });
+	  
+     $(".font-change2").each(function () {
+         var charLength = $(this).text().length;
+			
+         if (charLength >= 20) $(this).css("font-size", "16px");
+     });
+ });
--- public/js/font-change.js
+++ public/js/font-change.js
@@ -1,24 +1,14 @@
 /*custom.min*/
-	
-    $(document).ready(function () {
-
-        $(".font-change").each(function () {
-
-            var charLength = $(this).text().length;
-
-            if (charLength >= 20) $(this).css("font-size", "12px");
-        });
-
-    });
-
-
-    $(document).ready(function () {
-
-        $(".font-change2").each(function () {
-
-            var charLength = $(this).text().length;
-
-            if (charLength >= 20) $(this).css("font-size", "16px");
-        });
-
-    });
+ $(document).ready(function () {
+     $(".font-change").each(function () {
+         var charLength = $(this).text().length;
+			
+         if (charLength >= 20) $(this).css("font-size", "12px");
+     });
+	  
+     $(".font-change2").each(function () {
+         var charLength = $(this).text().length;
+			
+         if (charLength >= 20) $(this).css("font-size", "16px");
+     });
+ });
