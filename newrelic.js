9edffc264bf8f4d067ce7ab09ec2e1e2dbd76da6
Matej Kramny <matejkramny@gmail.com>
2014-01-23 13:16:16 +0000

    Fix newrelic logging

--- newrelic.js
+++ newrelic.js
@@ -21,7 +21,8 @@ exports.config = {
      * issues with the agent, 'info' and higher will impose the least overhead on
      * production applications.
      */
-    level : 'info'
+    level : 'info',
+	 path: 'STDOUT'
   },
   rules: {
 	  ignore: [
