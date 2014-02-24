dbec9ebaf86f59b9cbdcd9d1be9bb7de90c6a299
Alan Campbell <Alan.Campbell@castawaylabs.com>
2014-02-24 00:06:24 -0500

    Add a section for My Events to user home.
    Signed-off-by: Alan Campbell <Alan.Campbell@castawaylabs.com>

--- routes/home.js
+++ routes/home.js
@@ -19,7 +19,7 @@ exports.display = function (req, res) {
 			.select('name start end address venue_name avatar')
 			.exec(function(err, evs) {
 			if (err) throw err;
-		
+			
 			models.Event.find(query).count(function(err, total) {
 				if (err) throw err;
 			
