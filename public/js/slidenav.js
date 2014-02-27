203d23d7caeaa3c536b32bedfda2a54455cf44e3
Patryk Brzeziński <info@studiograficzne24.pl>
2014-02-27 21:33:06 +0100

    some responsive changes

--- public/js/slidenav.js
+++ public/js/slidenav.js
@@ -2,7 +2,7 @@ $(document).ready(function(){
         $("#open").click(function(){
             if ($("#slidenav").is(':hidden'))
                 $("#slidenav").show().animate({
-                top: '250'
+                top: '257'
             }, 500);
         
             else{
