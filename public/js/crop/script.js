44bb7ab348ee483462c00603273b4e11c7685fe1
Patryk Brzeziński <info@studiograficzne24.pl>
2014-04-08 22:55:28 +0200

    cropping tool

--- public/js/crop/script.js
+++ public/js/crop/script.js
@@ -1,238 +1,114 @@
-/**
- *
- * HTML5 Image uploader with Jcrop
- *
- * Licensed under the MIT license.
- * http://www.opensource.org/licenses/mit-license.php
- * 
- * Copyright 2012, Script Tutorials
- * http://www.script-tutorials.com/
- */
+//Make global variables for selected image for further usage
+var selectImgWidth,selectImgHeight,jcrop_api, boundx, boundy,isError=false;
+$(document).ready(function(){
+    $("#file_browse").change(function(){
+        var previewId = document.getElementById('load_img');
+        var thumbId = document.getElementById('thumb');
+        previewId.src = '';
+        $('#image_div').hide();
+        var flag = 0;
+        
+        // Get selected file parameters
+        var selectedImg = $('#file_browse')[0].files[0];
+        
+        //Check the select file is JPG,PNG or GIF image
+        var regex = /^(image\/jpeg|image\/png)$/i;
+        if (! regex.test(selectedImg.type)) {
+            $('.error').html('Please select a valid image file (jpg and png are allowed)').fadeIn(500);
+            flag++;
+            isError = true;
+        }
+        
+        // Check the size of selected image if it is greater than 250 kb or not
+        else if (selectedImg.size > 250 * 1024) {
+            $('.error').html('The file you selected is too big. Max file size limit is 250 KB').fadeIn(500);
+            $('.info-m').html('Select part of image you want to crop').fadeIn(500);
+            flag++;
+            isError = true;
+        }
+        
+        if(flag==0){
+        isError=false;
+        $('.error').hide(); //if file is correct then hide the error message
+        $('.info-m').show(); //if file is correct then hide the error message
+        
 
-var examples = [];
+        // Preview the selected image with object of HTML5 FileReader class
+        // Make the HTML5 FileReader Object
+        var oReader = new FileReader();
+            oReader.onload = function(e) {
 
-                    examples.push(function (){
-                        $('#userpic').fileapi({
-                            url: 'http://rubaxa.org/FileAPI/server/ctrl.php',
-                            accept: 'image/*',
-                            imageSize: { minWidth: 256, minHeight: 171 },
-                            elements: {
-                                active: { show: '.js-upload', hide: '.js-browse' },
-                                preview: {
-                                    el: '.js-preview',
-                                    width: 256 ,
-                                    height: 171
-                                },
-                                progress: '.js-progress'
-                            },
-                            onSelect: function (evt, ui){
-                                var file = ui.files[0];
+            // e.target.result is the DataURL (temporary source of the image)
+                thumbId.src = previewId.src=e.target.result;
+                
+                // FileReader onload event handler
+                previewId.onload = function () {
 
-                                if( file ){
-                                    $('#popup').modal({
-                                        closeOnEsc: true,
-                                        closeOnOverlayClick: false,
-                                        onOpen: function (overlay){
-                                            $(overlay).on('click', '.js-upload', function (){
-                                                $.modal().close();
-                                                $('#userpic').fileapi('upload');
-                                            });
+                // display the image with fading effect
+                $('#image_div').fadeIn(500);
+                selectImgWidth = previewId.naturalWidth; //set the global image width
+                selectImgHeight = previewId.naturalHeight;//set the global image height
+                
+                // Create variables (in this scope) to hold the Jcrop API and image size
+               
+                // destroy Jcrop if it is already existed
+                if (typeof jcrop_api != 'undefined') 
+                    jcrop_api.destroy();
 
-                                            $('.js-img', overlay).cropper({
-                                                file: file,
-                                                bgColor: '#ccc',
-                                                maxSize: [$(window).width()-100, $(window).height()-100],
-                                                minSize: [256, 171],
-                                                selection: '90%',
-                                                onSelect: function (coords){
-                                                    $('#userpic').fileapi('crop', file, coords);
-                                                }
-                                            });
-                                        }
-                                    }).open();
-                                }
-                            }
-                        });
-                    });
+                // initialize Jcrop Plugin on the selected image
+                $('#load_img').Jcrop({
+                    minSize: [33, 17], // min crop size
+                    aspectRatio: 312/158, //keep aspect ratio
+                    bgFade: true, // use fade effect
+                    bgOpacity: .3, // fade opacity
+                    onChange: showThumbnail,
+                    onSelect: showThumbnail
+                }, function(){
 
-jQuery(function ($){
-            var $blind = $('.splash__blind');
+                    // use the Jcrop API to get the real image size
+                    var bounds = this.getBounds();
+                    boundx = bounds[0];
+                    boundy = bounds[1];
 
-            $('.splash')
-                .mouseenter(function (){
-                    $('.splash__blind', this)
-                        .animate({ top: -10 }, 'fast', 'easeInQuad')
-                        .animate({ top: 0 }, 'slow', 'easeOutBounce')
-                    ;
-                })
-                .click(function (){
-                    if( !FileAPI.support.media ){
-                        $blind.animate({ top: -$(this).height() }, 'slow', 'easeOutQuart');
-                    }
+                    // Store the Jcrop API in the jcrop_api variable
+                    jcrop_api = this;
+                });
+            };
+        };
 
-                    FileAPI.Camera.publish($('.splash__cam'), function (err, cam){
-                        if( err ){
-                            alert("Unfortunately, your browser does not support webcam.");
-                        } else {
-                            $('.splash').off();
-                            $blind.animate({ top: -$(this).height() }, 'slow', 'easeOutQuart');
-                        }
-                    });
-                })
-            ;
+        // read selected file as DataURL
+        oReader.readAsDataURL(selectedImg);
+    }
+    })
+})
 
-
-
-
-
-            $('body').on('click', '[data-tab]', function (evt){
-                evt.preventDefault();
-
-                var el = evt.currentTarget;
-                var tab = $.attr(el, 'data-tab');
-                var $example = $(el).closest('.example');
-
-                $example
-                    .find('[data-tab]')
-                        .removeClass('active')
-                        .filter('[data-tab="'+tab+'"]')
-                            .addClass('active')
-                            .end()
-                        .end()
-                    .find('[data-code]')
-                        .hide()
-                        .filter('[data-code="'+tab+'"]').show()
-                ;
-            });
-
-
-            function _getCode(node, all){
-                var code = FileAPI.filter($(node).prop('innerHTML').split('\n'), function (str){ return !!str; });
-                if( !all ){
-                    code = code.slice(1, -2);
-                }
-
-                var tabSize = (code[0].match(/^\t+/) || [''])[0].length;
-
-                return $('<div/>')
-                    .text($.map(code, function (line){
-                        return line.substr(tabSize).replace(/\t/g, '   ');
-                    }).join('\n'))
-                    .prop('innerHTML')
-                        .replace(/ disabled=""/g, '')
-                        .replace(/&amp;lt;%/g, '<%')
-                        .replace(/%&amp;gt;/g, '%>')
-                ;
-            }
-
-
-            // Init examples
-            FileAPI.each(examples, function (fn){
-                fn();
-            });
-        });
-
-// convert bytes into friendly format
-function bytesToSize(bytes) {
-    var sizes = ['Bytes', 'KB', 'MB'];
-    if (bytes == 0) return 'n/a';
-    var i = parseInt(Math.floor(Math.log(bytes) / Math.log(1024)));
-    return (bytes / Math.pow(1024, i)).toFixed(1) + ' ' + sizes[i];
-};
-
-// check for selected crop region
-function checkForm() {
-    if (parseInt($('#w').val())) return true;
-    $('.error').html('Please select a crop region and then press Upload').show();
-    return false;
-};
-
-// update info by cropping (onChange and onSelect events handler)
-function updateInfo(e) {
+function showThumbnail(e)
+{
+    var rx = 312 / e.w; //155 is the width of outer div of your profile pic
+    var ry = 158 / e.h; //190 is the height of outer div of your profile pic
+    $('#w').val(e.w);
+    $('#h').val(e.h);
+    $('#w1').val(e.w);
+    $('#h1').val(e.h);
     $('#x1').val(e.x);
     $('#y1').val(e.y);
     $('#x2').val(e.x2);
     $('#y2').val(e.y2);
-    $('#w').val(e.w);
-    $('#h').val(e.h);
-};
+    $('#thumb').css({
+        width: Math.round(rx * selectImgWidth) + 'px',
+        height: Math.round(ry * selectImgHeight) + 'px',
+        marginLeft: '-' + Math.round(rx * e.x) + 'px',
+        marginTop: '-' + Math.round(ry * e.y) + 'px'
+    });
+}
 
-// clear info by cropping (onRelease event handler)
-function clearInfo() {
-    $('.info #w').val('');
-    $('.info #h').val('');
-};
-
-function fileSelectHandler() {
-
-    // get selected file
-    var oFile = $('#avatar')[0].files[0];
-
-    // hide all errors
-    $('.error').hide();
-
-    // check for image type (jpg and png are allowed)
-    var rFilter = /^(image\/jpeg|image\/png)$/i;
-    if (! rFilter.test(oFile.type)) {
-        $('.error').html('Please select a valid image file (jpg and png are allowed)').show();
-        return;
+function validateForm(){
+    if ($('#file_browse').val()=='') {
+        $('.error').html('Please select an image').fadeIn(500);
+        return false;
+    }else if(isError){
+        return false;
+    }else {
+        return true;
     }
-
-    // check for file size
-    if (oFile.size > 250 * 1024) {
-        $('.error').html('You have selected too big file, please select a one smaller image file').show();
-        return;
-    }
-
-    // preview element
-    var oImage = document.getElementById('preview');
-
-    // prepare HTML5 FileReader
-    var oReader = new FileReader();
-        oReader.onload = function(e) {
-
-        // e.target.result contains the DataURL which we can use as a source of the image
-        oImage.src = e.target.result;
-        oImage.onload = function () { // onload event handler
-
-            // display step 2
-            $('.step2').fadeIn(500);
-
-            // display some basic image info
-            var sResultFileSize = bytesToSize(oFile.size);
-            $('#filesize').val(sResultFileSize);
-            $('#filetype').val(oFile.type);
-            $('#filedim').val(oImage.naturalWidth + ' x ' + oImage.naturalHeight);
-
-            // Create variables (in this scope) to hold the Jcrop API and image size
-            var jcrop_api, boundx, boundy;
-
-            // destroy Jcrop if it is existed
-            if (typeof jcrop_api != 'undefined') 
-                jcrop_api.destroy();
-
-            // initialize Jcrop
-            $('#preview').Jcrop({
-                minSize: [50, 32], // min crop size
-                aspectRatio: 150/100,
-                bgFade: true, // use fade effect
-                bgOpacity: .3, // fade opacity
-                onChange: updateInfo,
-                onSelect: updateInfo,
-                onRelease: clearInfo
-            }, function(){
-
-                // use the Jcrop API to get the real image size
-                var bounds = this.getBounds();
-                boundx = bounds[0];
-                boundy = bounds[1];
-
-                // Store the Jcrop API in the jcrop_api variable
-                jcrop_api = this;
-            });
-        };
-    };
-
-    // read selected file as DataURL
-    oReader.readAsDataURL(oFile);
 }
