87e78c1c6fec10f5b5db01255f6dc578a57cf239
technosurgepk <humayun@technosurge.com>
2014-09-03 17:00:14 +0500

    Update list.js
    Nearest events on landing page. Added new method 'listNearLandingEvents'

--- routes/event/list.js
+++ routes/event/list.js
@@ -214,3 +214,116 @@ exports.listNearEvents = function (req, res) {
 		}
 	);
 }
+
+exports.listNearLandingEvents = function (req, res) {
+	var lat = parseFloat(req.query.lat)
+		, lng = parseFloat(req.query.lng)
+		, limit = parseInt(req.query.limit)
+		, distance = req.query.distance
+		, htmlIsEnabled = Boolean(req.query.html)
+		, page = parseInt(req.query.page)
+
+	if (!lat || !lng) {
+		// render a blank page, and tell it to ask user for browser positioning
+		res.format({
+			html: function() {
+				res.locals.moment = moment;
+				res.render('search/landing', { nearby: true, search: { results: [] }, moment:moment, title: "Events nearby" })
+			},
+			json: function() {
+				res.send({
+					events: [],
+					pagename: "Events near you"
+					// no lat&lng supplied!
+				})
+			}
+		})
+		return;
+	}
+
+	
+	if (limit == NaN) {
+		limit = 10;
+	}
+	
+	var query = {
+		'geo': {
+			$geoWithin: {
+				$center: [
+					[lng, lat],
+					0.2
+				]
+			}
+		}
+	};
+	models.Geolocation.find(query).populate({
+		path: 'event',
+		select: 'name description start end address venue_name avatar source',
+		match: {
+			deleted: false,
+			privateEvent: {
+				$ne: true
+			},
+			start: { $gte: Date.now() }
+		},
+	}).limit(limit)
+		.skip(limit * page)
+		.exec(function(err, geos) {
+			if (err) throw err;
+			console.log(geos);
+			if (geos) {
+				var events = [];
+				
+				for (var i = 0; i < geos.length; i++) {
+					if (geos[i].event == null) {
+						continue;
+					}
+					if (geos[i].event.deleted != true) {
+						geos[i].event.geo = geos[i].geo;
+						events.push(geos[i].event);
+					}
+				}
+				
+				// May slow the app down.. Mongoose does not seem to support sub-document population (event.avatar)
+				async.each(geos, function(geo, cb) {
+					if (geo.event)
+						geo.event.populate('avatar', cb);
+					else
+						cb(null)
+				}, function(err) {
+					res.format({
+						html: function() {
+							res.render('search/landing', { nearby: true, search: { results: events }, moment:moment, title: "Events nearby" })
+						},
+						json: function() {
+							if (events.length > 0 && htmlIsEnabled) {
+								models.Geolocation.find(query).count(function(err, count) {
+									var html = jade.renderFile(config.path + '/views/event/listBlank.jade', {
+										moment: moment,
+										events: events,
+										eventsTotal: count,
+										eventsSkip: limit * page
+									});
+
+									res.send({
+										html: html,
+										pagename: "Events near you",
+										events: events
+									});
+								})
+
+								return;
+							}
+
+							res.send({
+								events: events,
+								pagename: "Events near you"
+							})
+						}
+					})
+				})
+				
+			}
+		}
+	);
+}
