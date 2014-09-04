0459fd73ad0a1831de69e5cf13002560544712b9
ahmadps19 <bahmad@powersoft19.com>
2014-09-04 21:33:37 +0500

    Update apiadd.js
    PS19
    - Updated Event Add API.

--- routes/ws/apiroutes/apievent/apiadd.js
+++ routes/ws/apiroutes/apievent/apiadd.js
@@ -4,20 +4,32 @@ var models = require('../../../../models'),
 	config = require('../../../../config')
 
 exports.router = function (app) {
-	app.get('/api/event/add', util.authorized, addEvent)
-		.post('/api/event/add', util.authorized, doAddEvent)
+	app.get('/api/event/add', util.authorized, addEventAPI)
+		.post('/api/event/add', util.authorized, doAddEventAPI)
 		.post('/api/event/add/avatar', util.authorized, uploadAvatarAsync)
 		.get('/api/event/:avatarid/avatar/remove', util.authorized, removeAvatar)
 }
 
-function addEvent (req, res) {
+function addEventAPI (req, res) {
 	res.render('event/add', { title: "Add Event" })
 }
 
-function doAddEvent (req, res) {
+function doAddEventAPI (req, res) {
+	
+	//console.log("/api/event/add".red);
+	//console.log("##############".red);
+	//console.log(req.body);
+	//console.log("##############".red);
+	
+	models.User.findById(req.body._id , function(err, user) {
+		
+	//console.log("##############".red);
+	//console.log(user);
+	//console.log("##############".red);
+	
 	var newEvent = new models.Event({});
 	
-	newEvent.edit(req.body, req.user, req.files, function(err, ev) {
+	newEvent.edit(req.body, user, req.files, function(err, ev) {
 		if (err && err.length > 0) {
 			res.format({
 				json: function() {
@@ -38,6 +50,7 @@ function doAddEvent (req, res) {
 				})
 			}
 		});
+	});
 	});
 }
 
