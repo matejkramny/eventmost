fa13832b0481af2b1643bb7f213bb640993812dd
ahmadps19 <bahmad@powersoft19.com>
2014-09-19 19:33:50 +0500

    Update apiprofiles.js
    PS19
    - Saved Profiles
    - Profile Savers
    done.

--- routes/ws/apiroutes/apiinbox/apiprofiles.js
+++ routes/ws/apiroutes/apiinbox/apiprofiles.js
@@ -6,25 +6,48 @@ var fs = require('fs')
 	, transport = require('../../../../app').transport
 
 exports.router = function (app) {
-	app.get('/api/inbox/savedProfiles', exports.showSavedProfiles)
+	app.post('/api/inbox/savedProfiles', exports.showSavedProfilesAPI)
+	app.post('/api/inbox/saverProfiles', exports.showSaverProfilesAPI)
 }
 
-exports.showSavedProfiles = function (req, res) {
-	models.User.find({savedProfiles: req.user._id })
-		.exec(function(err, saver) {
-		if (err) throw err;
-		if (saver) {
-			res.format({
-				html: function() {
-					res.render('inbox/profiles', { profileSavers: saver, pageName: "Saved Profiles", title: "Saved Profiles" });
-				},
-				json: function() {
-					res.send({
-						profileSavers: saver,
-						pagename: "Saved Profiles"
-					})
-				}
-			})
-		}
-	})
+exports.showSavedProfilesAPI = function (req, res) {
+	
+	models.User.findOne({_id:req.body._id} , function(err, current_user) 
+	{
+		var query = {'_id': {$in: current_user.savedProfiles}};
+		
+		models.User.find(query)
+		.exec(function(err, savedprofiles) {
+			if (err) throw err;
+			if (savedprofiles) {
+				res.format({
+					json: function() {
+						res.send({
+							profilessaved: savedprofiles
+						});
+					}
+				});
+			}
+		});
+	});
+}
+
+exports.showSaverProfilesAPI = function (req, res) {
+	
+		var query = {savedProfiles: req.body._id};
+		
+		models.User.find(query)
+		.exec(function(err, savedprofiles) {
+			if (err) throw err;
+			if (savedprofiles) {
+				
+				res.format({
+					json: function() {
+						res.send({
+							profilesavers: savedprofiles
+						});
+					}
+				});
+			}
+		});
 }
