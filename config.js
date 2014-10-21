df7c76d7b790e74c9a509a01621becb99430834f
ahmadps19 <bahmad@powersoft19.com>
2014-10-21 19:29:35 +0500

    Update config.js

--- config.js
+++ config.js
@@ -84,7 +84,7 @@ if (!(mode == "test")) {
 }
 
 exports.version = '0.2.8';
-exports.host = credentials.host ? credentials.host : "eventmost.com";
+exports.host = credentials.host ? credentials.host : "dev.eventmost.com";
 exports.mode = mode;
 exports.sessionKey = 'em_sess';
 exports.sessionSecret = credentials.session_secret
