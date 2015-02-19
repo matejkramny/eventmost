97ff3b30d689dd13c25f6141ac977d6cf613fe8c
Haseeb Khilji <haseebkhilji@gmail.com>
2015-02-20 01:02:35 +0500

    Added Animations on Landing Page

--- public/js/landingpage.js
+++ public/js/landingpage.js
@@ -1,20 +1,62 @@
+var counter = 1;
 $(document).ready(function(){
+
 	setTimeout(function() {
     	$("#videoContainer")[0].pause();
     	showText();
-    	showForm();
+    	//showForm();
 
     }, 3000);
 	//alert("im ready");
 });
 
+
 function showText(){
 	$( "#textbanner" ).fadeIn( 3000, function() {
-    	$('html, body').animate({
+		
+      	image_animate();
+      	
+    	/*$('html, body').animate({
 		    scrollTop: $("#videoContainer").offset().top
-		}, 2000);
+		}, 2000);*/
   	});
 }
+
+function image_animate() {
+	
+	if(counter == 1) {
+		console.log("first log moving");
+		$('#plane').show();
+
+		setTimeout(function() {
+			$('#plane').animate({left: -400, top: 200}, 7000, 'swing', function(){
+				$('#plane2').show();
+				counter++;
+				image_animate();
+			});
+		}, 4000);
+	}
+	if(counter == 2) {
+		console.log("second log moving");
+		setTimeout(function(){
+			$('#plane2').animate({top: 200}, 7000, 'swing', function(){
+				$('#plane3').show();
+				counter++;
+				image_animate();
+			});
+		}, 4000);
+	}
+	if(counter == 3) {
+		console.log("third log moving");
+		setTimeout(function(){
+			$('#plane3').animate({right: -400, top: 200}, 7000, 'swing', function(){
+				counter++;
+				showForm();
+			});
+		}, 4000);
+	}
+}
+
 
 function showForm(){
 	$( "#textbanner" ).fadeOut( 3000, function() {
@@ -40,10 +82,9 @@ function showForm(){
 			setTimeout(function() {
 		    	$('html, body').animate({
 				    scrollTop: $("#arrangeby").offset().top
-				}, 5000);
+				}, 500);
 
 		    }, 3000);
-			
 		});
 
   	});
