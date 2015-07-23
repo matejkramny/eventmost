aa48a9e138842b7cd9543d2264afde9d689e231e
Sulaiman <sulaiman.malik@gmail.com>
2015-07-24 03:02:30 +0500

    push registration model

--- models/index.js
+++ models/index.js
@@ -22,5 +22,6 @@ module.exports = {
 	UserTakeoverRequest: require('./UserTakeoverRequest').UserTakeoverRequest,
 	Transaction: require('./Transaction').Transaction,
 	PasswordReset: require('./PasswordReset').PasswordReset,
-	Contact: require('./Contact').Contact
+	Contact: require('./Contact').Contact,
+	DeviceUsers: require('./DeviceUsers').DeviceUsers
 }
