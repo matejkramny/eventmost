4f4c9c00b9bac7a17837c42576d4575211f9fd36
Sulaiman <sulaiman.malik@gmail.com>
2015-07-25 02:31:38 +0500

    initializing sendIOSPushNotification in the router

--- routes/API/index.js
+++ routes/API/index.js
@@ -1,6 +1,8 @@
-var registerDeviceForPush = require('./registerDeviceForPush');
+var registerDeviceForPush = require('./registerDeviceForPush'),
+    sendIOSPushNotification = require('./sendIOSPushNotification');
 	
 	
 exports.router = function(app) {
     registerDeviceForPush.router(app);
+    sendIOSPushNotification.router(app);
 }
