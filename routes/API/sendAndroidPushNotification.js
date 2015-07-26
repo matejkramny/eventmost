b2290f6ea873216b889ab383a8b7925fbbba5f2f
Sulaiman <sulaiman.malik@gmail.com>
2015-07-27 00:30:48 +0500

    Android Push Notification

--- routes/API/sendAndroidPushNotification.js
+++ routes/API/sendAndroidPushNotification.js
@@ -2,6 +2,7 @@ var gcm = require('node-gcm');
 var models = require('../../models');
 var mongoose = require('mongoose');
 var deviceUsers = models.DeviceUsers;
+var sendPushObj = require('../../routes/core/sendPush');
 
 exports.router = function (app) {
     app.post('/api/sendAndroidPush',sendAndroidPush)
@@ -25,40 +26,7 @@ function sendAndroidPush(req,res) {
             }
         })
         return;
+    } else {
+        sendPushObj.sendPush(senderId,receiverId,message,'Android');
     }
-    deviceUsers.findOne({deviceUser:receiverId,deviceType:'Android'}).select({deviceToken:1}).exec(function (err, token){
-        if (err) return err;
-
-        if(token) {
-            message.addNotification('title', 'You have a new message from senderId ' + senderId);
-            message.addData('key1', message);
-
-            var regIds = [];
-
-            regIds.push(token.deviceToken);
-
-            //TODO: Need an android app key
-            var sender = new gcm.Sender('YOUR_API_KEY_HERE');
-
-            sender.send(message, regIds, function (err, result) {
-                if(err) console.error(err);
-                else    console.log(result);
-            });
-
-            sender.sendNoRetry(message, regIds, function (err, result) {
-                if(err) console.error(err);
-                else    console.log(result);
-            });
-        } else {
-            res.format({
-                json: function() {
-                    res.send({
-                        status: 404,
-                        messages: "No token found against the receiverId"
-                    })
-                }
-            })
-        }
-    })
-
 }
