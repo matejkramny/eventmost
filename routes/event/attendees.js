62e32e00e544e867cbefa7e6c23c0d8ca9b57b3f
Haseeb Khilji <haseebkhilji@gmail.com>
2015-09-21 21:07:38 +0500

    Passed variable to fix the layout for attendee profile

--- routes/event/attendees.js
+++ routes/event/attendees.js
@@ -197,7 +197,7 @@ function showAttendee (req, res) {
 	res.locals.theUser = theAttendee.user;
 	res.locals.theAttendee = theAttendee;
 	res.locals.saved = false;
-	res.render('user', { title: theAttendee.user.getName() + " In " + res.locals.ev.name });
+	res.render('user', { title: theAttendee.user.getName() + " In " + res.locals.ev.name, feedbackpage: true });
 }
 
 function removeAttendee (req, res) {
