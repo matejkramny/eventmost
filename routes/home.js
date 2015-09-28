ed750d8d4cae0c3c058b9986d5d8521a70e4e6de
Haseeb Khilji <haseebkhilji@gmail.com>
2015-09-29 04:00:16 +0500

    Fixed sort query

--- routes/home.js
+++ routes/home.js
@@ -19,7 +19,7 @@ exports.display = function (req, res) {
 		sort = {'start': -1};
 	}
 	
-	models.Event.find(query).limit(100).sort(sort).skip(skip).populate('avatar').exec(function(err, evs) {
+	models.Event.find(query).sort(sort).limit(100).skip(skip).populate('avatar').exec(function(err, evs) {
 
 		//Cleaning up the description...
 		evs.forEach(function(entry) {
