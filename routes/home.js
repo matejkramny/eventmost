e51afddf947aa166bf01978f7f1cf72b2da230d2
Haseeb Khilji <haseebkhilji@gmail.com>
2015-10-01 02:20:02 +0500

    Debuggin sort query

--- routes/home.js
+++ routes/home.js
@@ -30,7 +30,7 @@ exports.display = function (req, res) {
 
 	console.log(sortby);
 	
-	models.Event.find(query).sort({'start': 'desc'}).limit(10).skip(skip).populate('avatar').exec(function(err, evs) {
+	models.Event.find(query).sort(sortby).limit(100).skip(skip).populate('avatar').exec(function(err, evs) {
 
 		//Cleaning up the description...
 		//console.log(evs);
