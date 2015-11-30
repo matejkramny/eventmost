8847534deb90c5bf2216c9e5cf049fc050ca63b5
Haseeb Khilji <haseebkhilji@gmail.com>
2015-11-30 17:35:33 +0500

    New Animations

--- public/js/landingpage.js
+++ public/js/landingpage.js
@@ -1,135 +1,49 @@
 var counter = 1;
 $(document).ready(function(){
-	$('#header').hide();
-	showText();
-
-	setTimeout(function() {
-    	//$("#videoContainer")[0].pause();
-    }, 7000);
-});
-
-function moveuptillvideo(){
-	$('html, body').animate({
-	    scrollTop: $("#videoContainer").offset().top
-	}, 500);
-}
-
-function showText(){
-	//$('#videoContainer')[0].scrollIntoView(true);
-	$( "#textbanner" ).fadeIn( 6000, function() {
-		
-      	image_animate();
-      	
-    	/*$('html, body').animate({
-		    scrollTop: $("#videoContainer").offset().top
-		}, 2000);*/
-  	});
-}
-
-function image_animate() {
-	
-	if(counter == 1) {
-		console.log("first log moving");
-		$('#plane').show();
-
-		setTimeout(function() {
-			$('#plane').animate({left: -400, top: 200}, 500, 'swing', function(){
-				$('#plane2').show();
-				counter++;
-				image_animate();
-			});
-		}, 4000);
-	}
-	if(counter == 2) {
-		console.log("second log moving");
-		setTimeout(function(){
-			$('#plane2').animate({top: 200}, 500, 'swing', function(){
-				$('#plane3').show();
-				counter++;
-				image_animate();
-			});
-		}, 4000);
-	}
-	if(counter == 3) {
-		console.log("third log moving");
-		setTimeout(function(){
-			$('#plane3').animate({right: -400, top: 200}, 500, 'swing', function(){
-				$("#videoContainer")[0].pause();
-				counter++;
-				showForm();
-			});
-		}, 4000);
-		
-	}
-}
-
-function moveUp(){
-	console.log(navigator.platform+"--");
-	$('#plane').animate({top: 50}, 1000, 'swing', function(){
-		
+	$('#header').css({"float": "left",
+				    "height": "85px",
+				    "position": "absolute",
+				    "width": "100%",
+				    "z-index": "99999"});
+	$('#textbanner2').css({"top": "90px"});
+	$('#searchbanner').css({"top": "150px"});
+	//$( "#textbanner" ).show();
+	$('#plane').show();
+	$('#plane').css({
+		left: -400,
+		top: 100
 	});
-	$('#plane2').animate({top: 50}, 1000, 'swing', function(){
-			
-		});
-	$('#plane3').animate({top: 50}, 1000, 'swing', function(){
-			});
-	
+	$('#plane2').show();
+	$('#plane2').css({
+		top: 100
+	});
+	$('#plane3').show();
+	$('#plane3').css({
+		right: -400,
+		top: 100
+	});
+
+	//$('#textbanner').show();
+	$('#textbanner2').show();
+	$('#textbanner2').html('<span style="margin: 0px; display: inline;">S </span><span style="margin: 0px; display: inline;">e </span><span style="margin: 0px; display: inline;">a </span><span style="margin: 0px; display: inline;">r </span><span style="margin: 0px; display: inline;">c </span><span style="margin: 0px; display: inline;">h </span><span style="margin: 0px; display: inline;">&nbsp; </span><span style="margin: 0px; display: inline;">e </span><span style="margin: 0px; display: inline;">v </span><span style="margin: 0px; display: inline;">e </span><span style="margin: 0px; display: inline;">n </span><span style="margin: 0px; display: inline;">t </span><span style="margin: 0px; display: inline;">s </span><span style="margin: 0px; display: inline;">&nbsp; </span><span style="margin: 0px; display: inline;">b </span><span style="margin: 0px; display: inline;">e </span><span style="margin: 0px; display: inline;">l </span><span style="margin: 0px; display: inline;">o </span><span style="margin: 0px; display: inline;">w </span><span style="margin: 0px; display: inline;">&nbsp; </span><span style="margin: 0px; display: inline;">o </span><span style="margin: 0px; display: inline;">r </span><span style="margin: 0px; display: inline;">&nbsp; </span><span style="margin: 0px; display: inline;">c </span><span style="margin: 0px; display: inline;">r </span><span style="margin: 0px; display: inline;">e </span><span style="margin: 0px; display: inline;">a </span><span style="margin: 0px; display: inline;">t </span><span style="margin: 0px; display: inline;">e </span><span style="margin: 0px; display: inline;">&nbsp; </span><span style="margin: 0px; display: inline;">y </span><span style="margin: 0px; display: inline;">o </span><span style="margin: 0px; display: inline;">u </span><span style="margin: 0px; display: inline;">r </span><span style="margin: 0px; display: inline;">&nbsp; </span><span style="margin: 0px; display: inline;">o </span><span style="margin: 0px; display: inline;">w </span><span style="margin: 0px; display: inline;">n </span>');
+	$('#searchbanner').show();
 	var diff = $("#eventswrapper").offset().top - ($("#plane").offset().top + 100);
 	if(navigator.platform == "Win32"){
 		diff = diff+30;
 	}
 	
 	
-	$('#eventswrapper').animate({top: -diff}, 1000, 'swing', function(){
+	$('#eventswrapper').animate({top: -diff}, 10, 'swing', function(){
 	  $('#header').show();
 	});
-
-	/*$('#eventswrapper').animate({top: -540}, 1000, 'swing', function(){
-		$('#header').show();
-			});*/
 	
-	
-}
 
-function showForm(){
-	$( "#textbanner" ).fadeOut( 200, function() {
-		$( "#textbanner2" ).show();
-    	var $el = $("#textbanner2"), text = $el.text(),
-		    words = text.split(""), html = "";
+	setTimeout(function() {
+    	$("#videoContainer")[0].pause();
+    }, 12000);
 
-		for (var i = 0; i < words.length; i++) {
-			var thisletter = words[i];
-			if(thisletter == '-'){
-				var newLetter = '&nbsp;';
-			}else{
-				var newLetter = thisletter;
-			}
-		    html += "<span style='margin: 0px'>" + newLetter + " </span>";
-		}
-
-		$el.html(html).children().hide().each(function(i){
-		  $(this).delay(i*20).fadeIn(20);
-		});
-
-		$( "#searchbanner" ).fadeIn( 1000, function() {
-			moveUp();
-			setTimeout(function() {
-				
-				$('#header').css({"float": "left",
-							    "height": "85px",
-							    "position": "absolute",
-							    "width": "100%",
-							    "z-index": "99999"});
-				$('#textbanner2').css({"top": "90px"});
-				$('#searchbanner').css({"top": "150px"});
-		    	
-
-		    }, 1000);
-		});
-
-  	});
-
-  	$("#loadSoonEvents").click(function (){
+    $("#loadSoonEvents").click(function (){
+  		alert('soon')
   		//var soonRes = '<div class="nspacer"></div><div>This is a test</div>';
         //$('#fetchedEvents').html(soonRes);
         $('#successmsg').html('<div style="margin-top:10px"><center><img src="../images/loading.gif" /></center></div>');
@@ -146,6 +60,7 @@ function showForm(){
   	});
 
   	$("#loadNearestEvents").click(function (){
+  		alert('near')
   		console.log(window.lastCoords);
   		var coords = window.lastCoords;
   		$('#successmsg').html('<div style="margin-top:10px"><center><img src="../images/loading.gif" /></center></div>');
@@ -167,4 +82,117 @@ function showForm(){
 		    
 		  });
   	});
+});
+
+function moveuptillvideo(){
+	$('html, body').animate({
+	    scrollTop: $("#videoContainer").offset().top
+	}, 10);
+}
+
+function image_animate() {
+	
+	if(counter == 1) {
+		console.log("first log moving");
+		$('#plane').show();
+
+		setTimeout(function() {
+			$('#plane').animate({left: -400, top: 200}, 10, 'swing', function(){
+				$('#plane2').show();
+				counter++;
+				image_animate();
+			});
+		}, 10);
+	}
+	if(counter == 2) {
+		console.log("second log moving");
+		setTimeout(function(){
+			$('#plane2').animate({top: 200}, 10, 'swing', function(){
+				$('#plane3').show();
+				counter++;
+				image_animate();
+			});
+		}, 10);
+	}
+	if(counter == 3) {
+		console.log("third log moving");
+		setTimeout(function(){
+			$('#plane3').animate({right: -400, top: 200}, 10, 'swing', function(){
+				$("#videoContainer")[0].pause();
+				counter++;
+				showForm();
+			});
+		}, 10);
+		
+	}
+}
+
+function moveUp(){
+	console.log(navigator.platform+"--");
+	$('#plane').animate({top: 50}, 10, 'swing', function(){
+		
+	});
+	$('#plane2').animate({top: 50}, 10, 'swing', function(){
+			
+		});
+	$('#plane3').animate({top: 50}, 10, 'swing', function(){
+			});
+	
+	var diff = $("#eventswrapper").offset().top - ($("#plane").offset().top + 100);
+	if(navigator.platform == "Win32"){
+		diff = diff+30;
+	}
+	
+	
+	$('#eventswrapper').animate({top: -diff}, 10, 'swing', function(){
+	  $('#header').show();
+	});
+
+	/*$('#eventswrapper').animate({top: -540}, 1000, 'swing', function(){
+		$('#header').show();
+			});*/
+	
+	
+}
+
+function showForm(){
+	$( "#textbanner" ).fadeOut( 10, function() {
+		$( "#textbanner2" ).show();
+    	var $el = $("#textbanner2"), text = $el.text(),
+		    words = text.split(""), html = "";
+
+		for (var i = 0; i < words.length; i++) {
+			var thisletter = words[i];
+			if(thisletter == '-'){
+				var newLetter = '&nbsp;';
+			}else{
+				var newLetter = thisletter;
+			}
+		    html += "<span style='margin: 0px'>" + newLetter + " </span>";
+		}
+
+		$el.html(html).children().hide().each(function(i){
+		  $(this).delay(i*20).fadeIn(20);
+		});
+
+		$( "#searchbanner" ).fadeIn( 10, function() {
+			moveUp();
+			setTimeout(function() {
+				
+				$('#header').css({"float": "left",
+							    "height": "85px",
+							    "position": "absolute",
+							    "width": "100%",
+							    "z-index": "99999"});
+				$('#textbanner2').css({"top": "90px"});
+				$('#searchbanner').css({"top": "150px"});
+		    	
+
+		    }, 10);
+		});
+
+  	});
+
+
+  	
 }
